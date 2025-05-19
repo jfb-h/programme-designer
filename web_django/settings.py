f6344f4c6385dbe5779387415bfbdf9109789a10
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2025-05-19 08:58:08 +0000

    settings; uv

--- web_django/settings.py
+++ web_django/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'PLACEHOLDER'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "eggroup.geographie.uni-muenchen.de",
+    "141.84.116.198"
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
